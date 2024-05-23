5efd8549261a743a2cc786748396c6386c6cdb50
Yavor Gornalov <y.gornalov@gmail.com>
2024-05-23 10:04:57 +0300

    MS-SQL-Retake-Exam-04-Apr-2023 --updated

--- DB-Exams/03-MS-SQL-Retake-Exam-04-Apr-2023/03-Update.sql
+++ DB-Exams/03-MS-SQL-Retake-Exam-04-Apr-2023/03-Update.sql
@@ -5,18 +5,26 @@ BEGIN TRANSACTION
 
 UPDATE Invoices
 	SET DueDate = '2023-04-01'
-WHERE IssueDate BETWEEN '2022-11-01' AND '2022-11-30'
+WHERE YEAR(IssueDate) = 2022 AND MONTH(IssueDate) = 11
+
+DECLARE @ClientsToUpdate TABLE 
+(
+	Id INT
+)
+INSERT INTO @ClientsToUpdate
+SELECT
+	Id
+FROM Clients
+WHERE Name LIKE '%CO%'
+
+DECLARE @NewAddresId INT = (SELECT 
+								a.Id
+							FROM Addresses AS a JOIN Countries AS c ON a.CountryId = c.Id
+							WHERE StreetName = 'Industriestr' AND StreetNumber = 79 AND
+							PostCode = 2353 AND City = 'Guntramsdorf' AND c.Name = 'Austria')
 
 UPDATE Clients
-	SET AddressId = 3 
-					--(SELECT 
-					--	a.Id
-					--FROM Addresses AS a JOIN Countries AS c ON a.CountryId = c.Id
-					--WHERE StreetName = 'Industriestr' AND StreetNumber = 79 AND
-					--PostCode = 2353 AND City = 'Guntramsdorf' AND c.Name = 'Austria')
-WHERE Id IN (SELECT
-				c.AddressId
-			FROM Clients AS c
-			WHERE c.Name LIKE '%CO%')
+	SET AddressId = @NewAddresId
+WHERE Id IN (SELECT Id FROM @ClientsToUpdate)
 
 ROLLBACK TRANSACTION
--- DB-Exams/03-MS-SQL-Retake-Exam-04-Apr-2023/10-Clients-by-Price.sql
+++ DB-Exams/03-MS-SQL-Retake-Exam-04-Apr-2023/10-Clients-by-Price.sql
@@ -3,11 +3,9 @@ GO
 
 SELECT
 	c.Name AS Client
-	, CAST(AVG(p.Price) AS INT)AS "Average Price"
+	, CAST(AVG(p.Price) AS INT) AS "Average Price"
 FROM Clients AS c
 JOIN ProductsClients AS pc ON pc.ClientId = c.Id
-JOIN Products AS p ON pc.ProductId = p.Id
-JOIN Vendors AS v ON p.VendorId = v.Id
-WHERE v.NumberVAT LIKE '%FR%'
-GROUP BY c.Name, v.NumberVAT
+JOIN Products AS p ON pc.ProductId = p.Id AND p.VendorId IN (SELECT Id FROM Vendors WHERE NumberVAT LIKE '%FR%')
+GROUP BY c.Name
 ORDER BY "Average Price", Client DESC
